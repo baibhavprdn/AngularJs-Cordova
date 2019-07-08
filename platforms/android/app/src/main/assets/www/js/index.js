24c823669f450de557e73af34e5d625cd26c73b0
Baibhav Pradhan <baibhavprdn371@gmail.com>
2019-07-08 17:28:51 +0545

    working read write file

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -3,13 +3,16 @@
    function configRoutes($routeProvider) {
       $routeProvider
          .when('/login', {
-            templateUrl: 'views/login.html'
+            templateUrl: 'views/login.html',
+            controller: 'loginController'
          })
          .when('/register', {
-            templateUrl: 'views/register.html'
+            templateUrl: 'views/register.html',
+            controller: 'loginController'
          })
          .otherwise({
-            redirectTo: 'views/login.html'
+            redirectTo: 'views/login.html',
+            controller: 'loginController'
          });
    }
 
@@ -21,47 +24,95 @@
             email: $scope.newuser.email,
             password: $scope.newuser.password
          });
+         console.log($scope.user);
+         writeToFile('userList.json', $scope.user);
+
+         var fileData = readFromFile('userList.json');
+
+         console.log(fileData);
       };
-      console.log($scope.user);
-      // writeToFile('userList.json', $scope.user);
+
+      $scope.loginUser = function () {
+         console.log("logged in user");
+      };
+   }
+
+   //cordova 
+   var errorHandler = function (fileName, e) {
+      var msg = '';
+
+      switch (e.code) {
+         case FileError.QUOTA_EXCEEDED_ERR:
+            msg = 'Storage quota exceeded';
+            break;
+         case FileError.NOT_FOUND_ERR:
+            msg = 'File not found';
+            break;
+         case FileError.SECURITY_ERR:
+            msg = 'Security error';
+            break;
+         case FileError.INVALID_MODIFICATION_ERR:
+            msg = 'Invalid modification';
+            break;
+         case FileError.INVALID_STATE_ERR:
+            msg = 'Invalid state';
+            break;
+         default:
+            msg = 'Unknown error';
+            break;
+      }
+
+      console.log('Error (' + fileName + '): ' + msg)
+   };
+
+   function readFromFile(fileName) {
+      var pathToFile = cordova.file.dataDirectory + fileName;
+      window.resolveLocalFileSystemURL(
+         pathToFile,
+         function (fileEntry) {
+            fileEntry.file(function (file) {
+               var reader = new FileReader();
+
+               reader.onloadend = function (e) {
+                  // cb(JSON.parse(this.result));
+                  console.log('Text is: ' + this.result);
+               };
+
+               reader.readAsText(file);
+            }, errorHandler.bind(null, fileName));
+         },
+         errorHandler.bind(null, fileName)
+      );
+   }
+
+   function writeToFile(fileName, data) {
+      data = JSON.stringify(data, null, '\t');
+      window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function (directoryEntry) {
+         directoryEntry.getFile(fileName, {
+            create: true
+         }, function (fileEntry) {
+            fileEntry.createWriter(function (fileWriter) {
+               fileWriter.onwriteend = function () {
+                  console.log("write of file " + fileName + " completed");
+               };
+
+               fileWriter.onerror = function (e) {
+                  console.log("write failed" + e.toString());
+               };
+
+               var blob = new Blob([data], {
+                  type: 'text/plain'
+               });
+               fileWriter.write(blob);
+               readFromFile('userList.json');
+            }, errorHandler.bind(null, fileName));
+         }, errorHandler.bind(null, fileName));
+      }, errorHandler.bind(null, fileName));
    }
 
    function onDeviceReady() {
       angular.bootstrap(document.body, ['loginApp']);
       //need to delay bootstrapping angular until deviceready fires
-
-      //cordova 
-      function errorHandler(fileName) {
-         console.log("error creating " + fileName);
-      }
-
-      function writeToFile(fileName, data) {
-         data = JSON.stringify(data, null, '\t');
-         window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function (directoryEntry) {
-            directoryEntry.getFile(fileName, {
-               create: true
-            }, function (fileEntry) {
-               fileEntry.createWriter(function (fileWriter) {
-                  fileWriter.onwriteend = function (e) {
-                     console.log("write of file " + fileName + " completed");
-                  };
-
-                  fileWriter.onerror = function (e) {
-                     console.log("write failed" + e.toString());
-                  };
-
-                  var blob = new Blob([data], {
-                     type: 'text/plain'
-                  });
-                  fileWriter.write(blob);
-               }, errorHandler.bind(null, fileName));
-            }, errorHandler.bind(null, fileName));
-         }, errorHandler.bind(null, fileName));
-      }
-
-      writeToFile('example.json', {
-         foo: 'bar'
-      });
    }
 
    var loginApp = angular.module('loginApp', ['ngRoute']);
--- www/js/index.js
+++ www/js/index.js
@@ -25,43 +25,90 @@
             password: $scope.newuser.password
          });
          console.log($scope.user);
+         writeToFile('userList.json', $scope.user);
+
+         var fileData = readFromFile('userList.json');
+
+         console.log(fileData);
       };
 
       $scope.loginUser = function () {
          console.log("logged in user");
       };
-      // writeToFile('userList.json', $scope.user);
    }
 
-   // //cordova 
-   // function errorHandler(fileName) {
-   //    console.log("error creating " + fileName);
-   // }
+   //cordova 
+   var errorHandler = function (fileName, e) {
+      var msg = '';
 
-   // function writeToFile(fileName, data) {
-   //    data = JSON.stringify(data, null, '\t');
-   //    window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function (directoryEntry) {
-   //       directoryEntry.getFile(fileName, {
-   //          create: true
-   //       }, function (fileEntry) {
-   //          fileEntry.createWriter(function (fileWriter) {
-   //             fileWriter.onwriteend = function () {
-   //                console.log("write of file " + fileName + " completed");
-   //             };
+      switch (e.code) {
+         case FileError.QUOTA_EXCEEDED_ERR:
+            msg = 'Storage quota exceeded';
+            break;
+         case FileError.NOT_FOUND_ERR:
+            msg = 'File not found';
+            break;
+         case FileError.SECURITY_ERR:
+            msg = 'Security error';
+            break;
+         case FileError.INVALID_MODIFICATION_ERR:
+            msg = 'Invalid modification';
+            break;
+         case FileError.INVALID_STATE_ERR:
+            msg = 'Invalid state';
+            break;
+         default:
+            msg = 'Unknown error';
+            break;
+      }
 
-   //             fileWriter.onerror = function (e) {
-   //                console.log("write failed" + e.toString());
-   //             };
+      console.log('Error (' + fileName + '): ' + msg)
+   };
 
-   //             var blob = new Blob([data], {
-   //                type: 'text/plain'
-   //             });
-   //             fileWriter.write(blob);
-   //          }, errorHandler.bind(null, fileName));
-   //       }, errorHandler.bind(null, fileName));
-   //    }, errorHandler.bind(null, fileName));
-   // }
+   function readFromFile(fileName) {
+      var pathToFile = cordova.file.dataDirectory + fileName;
+      window.resolveLocalFileSystemURL(
+         pathToFile,
+         function (fileEntry) {
+            fileEntry.file(function (file) {
+               var reader = new FileReader();
 
+               reader.onloadend = function (e) {
+                  // cb(JSON.parse(this.result));
+                  console.log('Text is: ' + this.result);
+               };
+
+               reader.readAsText(file);
+            }, errorHandler.bind(null, fileName));
+         },
+         errorHandler.bind(null, fileName)
+      );
+   }
+
+   function writeToFile(fileName, data) {
+      data = JSON.stringify(data, null, '\t');
+      window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function (directoryEntry) {
+         directoryEntry.getFile(fileName, {
+            create: true
+         }, function (fileEntry) {
+            fileEntry.createWriter(function (fileWriter) {
+               fileWriter.onwriteend = function () {
+                  console.log("write of file " + fileName + " completed");
+               };
+
+               fileWriter.onerror = function (e) {
+                  console.log("write failed" + e.toString());
+               };
+
+               var blob = new Blob([data], {
+                  type: 'text/plain'
+               });
+               fileWriter.write(blob);
+               readFromFile('userList.json');
+            }, errorHandler.bind(null, fileName));
+         }, errorHandler.bind(null, fileName));
+      }, errorHandler.bind(null, fileName));
+   }
 
    function onDeviceReady() {
       angular.bootstrap(document.body, ['loginApp']);
